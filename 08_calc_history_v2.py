a9ce5cc0eff33a48f0734e7616ad1ed714600d1d
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2021-06-22 09:55:21 +1200

    Current

--- 08_calc_history_v2.py
+++ 08_calc_history_v2.py
@@ -2,6 +2,7 @@
 # Only put in triangles and rectangles
 
 import math
+import pandas
 
 # number checker function goes here
 # Checks that it is not 0 and is a number
@@ -89,6 +90,7 @@ def rectangle():
         # Get base and height
         base = number_checker("What is the base? ", "Please enter a number above 0", float)
         height = number_checker("What is the height? ", "Please enter a number above 0", float)
+        recorded_info = ("Base: {} | Height: {}".format(base, height))
 
         # ------------- Calculations -------------
         area = base * height
@@ -103,10 +105,9 @@ def rectangle():
         # Print with appropriate shape name
         print("The area of your {} is {:.2f}".format(squ_or_rec, area))
         print("The perimeter of your {} is {:.2f}".format(squ_or_rec, perimeter))
-        print()
 
-        # return this until I put in a list for history
-        return ["Rectangle", base, height, area, perimeter]
+        # return this for use in history
+        return ["Rectangle", area, perimeter, recorded_info]
 
 # triangle function goes here
 # Figures out what the question gives and calculates the area and perimeter
@@ -128,6 +129,7 @@ def triangle():
             # Find Base and height
             base = number_checker("What is the base? ", "Please enter a number bigger than 0", float)
             height = number_checker("What is the height? ", "Please enter a number bigger than 0", float)
+            recorded_info = ("Base: {} | Height: {}".format(base, height))
                 
             area = 0.5 * base * height
             perimeter = "couldn't find"
@@ -141,6 +143,7 @@ def triangle():
             a = number_checker("What is the length of a? ", "Please enter a number bigger than 0", float)
             b = number_checker("What is the length of b? ", "Please enter a number bigger than 0", float)
             c = number_checker("What is the length of c? ", "Please enter a number bigger than 0", float)
+            recorded_info = ("A: {} | B: {} | C: {}".format(a, b, c))
 
             # Do Heron's Law and the sum of all the side lengths for perimeter
             s = (a + b + c) / 2
@@ -150,8 +153,142 @@ def triangle():
             print("The area of your triangle is {:.2f}".format(area))
             print("The perimeter of your triangle is {:.2f}".format(perimeter))
 
-        # return this until I put in a list for history
-        return ""
+        # return this for use in history
+        return ["Triangle", area, perimeter, recorded_info]
+
+# circle function goes here
+# Find the radius or diameter then calculate area and circumference
+def circle():
+    valid = False
+    while not valid:
+        
+        # Find what the user is given
+        info = input("What do you know about the circle [radius(r)] or [diameter(d)]? ").lower()
+        info_check = string_checker(info, [["r"], ["d"]], "Please say either 'r' for radius or 'd' for diameter")
+        if info_check == "invalid choice":
+            continue
+
+        # Diameter case
+        if info_check.lower() == "d":
+            diameter = number_checker("What is the diameter? ", "Please enter a number above 0", float)
+            # radius is diameter divided by 2
+            r = diameter / 2
+            recorded_info = ("Diameter: {}".format(diameter))
+        # Radius Case
+        else:
+            radius = number_checker("What is the radius? ", "Please enter a number above 0", float)
+            r = radius
+        recorded_info = ("Radius: {}".format(r))
+
+        # ------------- Calculations -------------
+        circumference = 2 * (math.pi) * r
+        area = (math.pi) * r**2
+
+        print("The area of your circle is {:.2f}".format(area))
+        print("The circumference of your circle is {:.2f}".format(circumference))
+        print()
+
+        # return this for use in history
+        return ["Circle", area, circumference, recorded_info]
+
+# parallelogram function goes here
+# Find the base, height and side length then calculate area and perimeter
+def parallelogram():
+    valid = False
+    while not valid:
+
+        # Find what the user is looking for
+        info = input("What do you want to find out [Area(a)] or [Perimeter(p)] or [Both(ap)]? ").lower()
+        info_check = string_checker(info, [["a "], [" p"], ["ap"]], "Please say either 'a' for area or 'p' for perimeter")
+        if info_check == "invalid choice":
+            continue
+        
+        # base is needed in all cases so it is outside the if statement
+        base = number_checker("What is the base? ", "Please enter a number above 0", float)
+        # Check if user wants area
+        if info_check[0] == "A":
+            height = number_checker("What is the height? ", "Please enter a number above 0", float)
+            # Check if user wants perimeter as well
+            if info_check[1] == "p":
+                side = number_checker("What is the side length? ", "Please enter a number above 0", float)
+                recorded_info = ("Base: {} | Height: {} | Side: {}".format(base, height, side))
+            # If only area
+            else:
+                side = 0
+                recorded_info = ("Base: {} | Height: {}".format(base, height))
+        # Perimeter scenario
+        else:
+            side = number_checker("What is the side length? ", "Please enter a number above 0", float)
+            height = 0
+            recorded_info = ("Base: {} | Side: {}".format(base, side))
+        
+        # ------------- Calculations -------------
+        area = base * height
+        perimeter = 2 * (side + base)
+
+        # Area
+        if height != 0:
+            print("The area of your parallelogram is {:.2f}".format(area))
+            # Perimeter as well
+            if side != 0:
+                print("The perimeter of your parallelogram is {:.2f}".format(perimeter))
+        # Perimeter only
+        else:
+            print("The perimeter of your parallelogram is {:.2f}".format(perimeter))
+        print()
+        # return this for use in history
+        return ["Parallelogram", area, perimeter, recorded_info]
+
+# trapezium function goes here
+# Find the base, height and side length then calculate area and perimeter
+def trapezium():
+    valid = False
+    while not valid:
+
+        # Find what the user is looking for
+        info = input("What do you want to find out [Area(a)] or [Perimeter(p)] or [Both(ap)]? ").lower()
+        info_check = string_checker(info, [["a "], [" p"], ["ap"]], "Please say either 'a' for area or 'p' for perimeter")
+        if info_check == "invalid choice":
+            continue
+        
+        # base is needed in all cases so it is outside the if statement
+        base_a = number_checker("What is the first base? ", "Please enter a number above 0", float)
+        base_b = number_checker("What is the second base? ", "Please enter a number above 0", float)
+        # Check if user wants perimeter
+        if info_check[1] == "p" or info_check[1] == "P":
+            side_c = number_checker("What is the first side length? ", "Please enter a number above 0", float)
+            side_d = number_checker("What is the second side length? ", "Please enter a number above 0", float)
+            # Check if user wants perimeter as well
+            if info_check[0] == "A":
+                height = number_checker("What is the height? ", "Please enter a number above 0", float)
+                recorded_info = ("A: {} | B: {} | C: {} | D: {} | Height: {}".format(base_a, base_b, side_c, side_d, height))
+            # If only area
+            else:
+                height = 0
+                recorded_info = ("A: {} | B: {} | C: {} | D: {}".format(base_a, base_b, side_c, side_d))
+        # Perimeter scenario
+        else:
+            height = number_checker("What is the height? ", "Please enter a number above 0", float)
+            side_c = 0
+            side_d = 0
+            recorded_info = ("A: {} | B: {} | Height: {}".format(base_a, base_b, height))
+        
+        # ------------- Calculations -------------
+        area = ((base_a + base_b)/2) * height
+        perimeter = base_a + base_b + side_c + side_d
+
+        # Area
+        if height != 0:
+            print("The area of your trapezium is {:.2f}".format(area))
+            # Perimeter as well
+            if side_c != 0:
+                print("The perimeter of your trapezium is {:.2f}".format(perimeter))
+        # Perimeter only
+        else:
+            print("The perimeter of your trapezium is {:.2f}".format(perimeter))
+        print()
+        # return this for use in history
+        return ["Trapezium", area, perimeter, recorded_info]
 
 # Main Routine
 history = []
@@ -169,9 +306,8 @@ while keep_going == "":
     history.append(result)
     keep_going = input("\nIf you want to continue press enter or any other key to quit: ")
 
-print("==================== History ====================\n")
-for item in history:
-    calc_num += 1
-    print("{}| Shape: {} | Area: {} | Perimeter: {}".format(calc_num, item[0], item[3], item[4]))
 print()
-    
+print("=============== History ===============\n")
+history_frame = pandas.DataFrame(history, columns=["Shape", "Area", "Perimeter", "Info"])
+
+print(history_frame)
