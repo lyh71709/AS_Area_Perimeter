a9d37a7e6740628b87f65621ceb259fd29565c38
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2021-06-30 15:10:09 +1200

    Current

--- 10_measure_units_v2.py
+++ 10_measure_units_v2.py
@@ -5,11 +5,15 @@ def convert_unit(desired_unit, answer):
     unit_list = ["km", "", "", "m", "", "cm", "mm"]
     number_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-    num = answer[:len(answer)-1]
-    unit = answer[len(answer)-1]
-    if num not in number_list:
-        num = answer[:len(answer)-2]
-        unit = answer[len(answer)-2:]
+    if answer[len(answer)-1] in number_list:
+        num = answer
+        unit = desired_unit
+    else:
+        num = answer[:len(answer)-1]
+        unit = answer[len(answer)-1]
+        if answer[len(answer)-2] not in number_list:
+            num = answer[:len(answer)-2]
+            unit = answer[len(answer)-2:]
 
     index1 = unit_list.index(unit)
     index2 = unit_list.index(desired_unit)
